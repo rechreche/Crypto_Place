6da67eb18d3fd6475669250d44fae30d1fd4a995
rechreche <135737371+rechreche@users.noreply.github.com>
2024-06-05 03:21:48 +0530

    complete Project

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+
+const footer = () => {
+  return (
+    <div className='footer'>
+        <p>Copyright @ 2024, Cryptoplace - All right Reserved.</p>
+        
+    </div>
+  )
+}
+
+export default footer
